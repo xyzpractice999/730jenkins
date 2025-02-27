aea90941fd4b6a7875c0a1e774764dda39eedde5
kishore <skill2automate@gmail.com>
2025-02-27 08:30:27 +0530

    updated project

---
+++ zproject99/src/test/java/tests/TestCase3.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.Test;
+
+public class TestCase3 {
+
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+	@Test
+	public void test3() {
+		System.out.println("test3");
+	}
+
+}
